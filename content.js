106eb8ed5d5f39aec109d1da639c14ab3d0eb632
adiiwill <gazdagadam02@gmail.com>
2024-08-06 17:28:46 +0200

    Added feature: Renaming shulker to the palette name

--- content.js
+++ content.js
@@ -1,7 +1,12 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getPalette") {
-      const paletteElements = document.getElementsByClassName("listText");
-      const palette = Array.from(paletteElements).map(el => el.textContent.trim());
-      sendResponse({palette: palette});
-    }
-  });
+  if (request.action === "getPalette") {
+    const paletteElements = document.getElementsByClassName("listText");
+    const palette = Array.from(paletteElements).map((el) =>
+      el.textContent.trim()
+    );
+    const title = document.querySelector(
+      "body > div.palettes > div > div > div.col-xl-9.col-lg-8.col-md-12 > div > div.col-xl-12.col-lg-12.paddingFix > div > div > div > div.col-xl-4 > h2"
+    ).innerText;
+    sendResponse({ palette: palette, title: title });
+  }
+});
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.getElementById("generate_btn").addEventListener("click", () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, {action: "getPalette"}, (response) => {
         if (response && response.palette) {
-          const command = generateCommand(response.palette);
+          const command = generateCommand(response.palette, response.title);
           document.getElementById("command_output").value = command;
           navigator.clipboard.writeText(command);
         }
@@ -10,9 +10,9 @@ document.getElementById("generate_btn").addEventListener("click", () => {
     });
   });
   
-  function generateCommand(palette) {
+  function generateCommand(palette, title) {
     const items = palette.slice(0, 9).map((block, index) => {
         return `{id:"minecraft:${block.toLowerCase().replace(/ /g, '_')}",Count:1b,Slot:${index}b}`;
       }).join(',');
-    return `/give @p minecraft:shulker_box{BlockEntityTag:{Items:[${items}]}}`;
+    return `/give @p minecraft:shulker_box{display:{Name:'{"text":"${title}"}'},BlockEntityTag:{Items:[${items}]}}`;
   }
